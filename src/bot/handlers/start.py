5d8b1d69f1bb9521e61e9891b75a201cc1bd0b62
Дмитрий Вислобоков <dmitryvislobokov@33.1.168.192.fryazino.net>
2025-09-11 17:21:17 +0300

    Somewhere fixed

--- src/bot/handlers/start.py
+++ src/bot/handlers/start.py
@@ -1,10 +1,16 @@
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
-
+from bot.keyboards import main_keyboard
+from backend.services import create_user
 
 start_router = Router()
 
 @start_router.message(Command("start"))
 async def cmd_start(message: Message):
-    await message.answer("Добро пожаловать в магазин меда!")
+    telegram_id = message.from_user.id
+    first_name = message.from_user.first_name
+    username = message.from_user.username
+    
+    await create_user(telegram_id, first_name, username)
+    await message.answer("Добро пожаловать в магазин меда! Выберете действие:", reply_markup=main_keyboard)
