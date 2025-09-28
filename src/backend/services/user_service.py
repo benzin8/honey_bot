bac0f4cb6207a3c88b160e0704b81fb05e2b4104
Дмитрий Вислобоков <dmitryvislobokov@MacBook-Air-Dmitrij.local>
2025-09-28 17:47:12 +0300

    Fixed: rewrite phone number to None

--- src/backend/services/user_service.py
+++ src/backend/services/user_service.py
@@ -4,28 +4,35 @@ from core.database import async_session
 
 async def create_user(telegram_id: int, name: str, username: str, phone: str = None):
     async with async_session() as session:
-        result = await session.execute( # Проверка на старого пользователя
+        """
+        Old user checker
+        """
+        result = await session.execute(
             select(User).where(User.telegram_id == telegram_id)
         )
-        user = result.scalar_one_or_none()
 
-        if user:
-            if phone and user.phone != phone:
+        user = result.scalar_one_or_none() # True или False
+        print(user)
+
+        """
+        Update data
+        """
+        if user is not None:
+            if phone is not None and user.phone != phone:
                 user.phone = phone
-            if name and user.name != name:
+            if user.name != name:
                 user.name = name
-            if username and user.username != username:
+            if user.username != username:
                 user.username = username
             await session.commit()
             return user
         else:
-            new_user = User(
+            user = User(
                 telegram_id = telegram_id,
                 name = name,
                 username = username
             )
             
-            session.add(new_user)
+            session.add(user)
             print(f"Новый пользователь: ID:{telegram_id}, Name: {name}, Username: {username}, Phone: {phone}")
             await session.commit()
-            return new_user
